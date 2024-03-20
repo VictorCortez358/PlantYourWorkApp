f6a00b5f2b74c0e961bdb78cf7838d08ee47e316
VictorCortez358 <00022120@uca.edu.sv>
2024-03-19 21:39:07 -0600

    ✨ Added Login and Register Layout

--- plant-your-work-api/src/auth/auth.controller.ts
+++ plant-your-work-api/src/auth/auth.controller.ts
@@ -1,5 +1,6 @@
-import { Body, Controller, Post, HttpCode, HttpStatus } from '@nestjs/common';
+import { Body, Controller, Post, Get, Request, HttpCode, HttpStatus, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { AuthGuard } from './auth.guard';
 import { User } from '@prisma/client';
 import { UsersService } from 'src/users/users.service';
 import { SignInDto } from './dto/SignInDto';
@@ -19,5 +20,11 @@ export class AuthController {
     @Post('register')
     async createUser(@Body() registerInDto: RegisterInDto): Promise<User> {
         return this.userService.createUser(registerInDto)
+    }
+
+    @UseGuards(AuthGuard)
+    @Get('profile')
+    getProfile(@Request() req) {
+        return req.user;
     }
 }
